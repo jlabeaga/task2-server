8253ce926bba9c16b15c19b6a40cee7e2d0ba846
jlabeaga <jlabeaga@hotmail.com>
2020-08-01 21:39:49 +0200

    added LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.confg';
 import { UsersModule } from './users/users.module';
 import { UserService } from './users/user.service';
 import { TasksModule } from './tasks/tasks.module';
+import { LoggerMiddleware } from './shared/middleware/logger-middleware';
 
 @Module({
   imports: [
@@ -15,4 +16,10 @@ import { TasksModule } from './tasks/tasks.module';
   ],
   providers: [UserService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware);
+    // .forRoutes('admin');
+  }
+}
